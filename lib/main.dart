c069dbdadd63f774b1d475780d50f9c900948d72
n3ling49 <noahdreiling@gmail.com>
2025-02-23 15:51:49 +0100

    add appwrite authentication logic and login/register form

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,73 @@ import 'package:bnbscout24/pages/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bnbscout24/utils/snackbar_service.dart';
+import 'package:bnbscout24/api/login_manager.dart';
+import 'package:bnbscout24/pages/login_register_page.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //check for existing appwrite session and landlord status
+  await LoginManager.checkSession();
+  await LoginManager.checkLandlord();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => LoginManager(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp();
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+//TODO: only make pages that actually require authentication hidden behind auth
+//create auth wrapper with callback?
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    final loginManager = Provider.of<LoginManager>(context);
+
+    return MaterialApp(
       scaffoldMessengerKey: SnackbarService.scaffoldMessengerKey,
       theme: ThemeData(
         scaffoldBackgroundColor: ColorPalette.white,
       ),
-      home: CustomTabBar(
-        items: [
-          CustomTabBarItem(
-              page: HomePage(),
-              tab_widget:
-                  FaIcon(FontAwesomeIcons.map, size: Sizes.navBarIconSize)),
-          CustomTabBarItem(
-              page: SearchPage(),
-              tab_widget: FaIcon(FontAwesomeIcons.magnifyingGlass,
-                  size: Sizes.navBarIconSize)),
-          CustomTabBarItem(
-              page: PropertiesPage(),
-              tab_widget:
-                  FaIcon(FontAwesomeIcons.house, size: Sizes.navBarIconSize)),
-          CustomTabBarItem(
-              page: ConversationsPage(),
-              tab_widget:
-                  FaIcon(FontAwesomeIcons.inbox, size: Sizes.navBarIconSize)),
-          CustomTabBarItem(
-              page: ProfilePage(),
-              tab_widget:
-                  FaIcon(FontAwesomeIcons.user, size: Sizes.navBarIconSize)),
-        ],
-      )));
+      home: loginManager.isLoggedIn
+          ? CustomTabBar(
+              items: [
+                CustomTabBarItem(
+                    page: HomePage(),
+                    tab_widget: FaIcon(FontAwesomeIcons.map,
+                        size: Sizes.navBarIconSize)),
+                CustomTabBarItem(
+                    page: SearchPage(),
+                    tab_widget: FaIcon(FontAwesomeIcons.magnifyingGlass,
+                        size: Sizes.navBarIconSize)),
+                if (loginManager.isLandlord)
+                  CustomTabBarItem(
+                      page: PropertiesPage(),
+                      tab_widget: FaIcon(FontAwesomeIcons.house,
+                          size: Sizes.navBarIconSize)),
+                CustomTabBarItem(
+                    page: ConversationsPage(),
+                    tab_widget: FaIcon(FontAwesomeIcons.inbox,
+                        size: Sizes.navBarIconSize)),
+                CustomTabBarItem(
+                    page: ProfilePage(),
+                    tab_widget: FaIcon(FontAwesomeIcons.user,
+                        size: Sizes.navBarIconSize)),
+              ],
+            )
+          : LoginRegisterPage(),
+    );
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:bnbscout24/components/form_input.dart';
 import 'package:bnbscout24/components/text_input.dart';
 import 'package:flutter/material.dart';
+import 'package:bnbscout24/api/login_manager.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,9 +24,10 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final loginManager = Provider.of<LoginManager>(context);
     return Container(
         child: FormInput(
-      label: "Hello!",
+      label: "Hello, ${loginManager.loggedInUser!.name}!",
       children: [TextInput()],
     ));
   }
